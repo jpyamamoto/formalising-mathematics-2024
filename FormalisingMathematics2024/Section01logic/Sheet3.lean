a35f8c5f97cde99fa03cd99f94c72dd89cc8d172
Juan Pablo Yamamoto <jpyamamoto@ciencias.unam.mx>
2024-01-24 05:29:22 +0000

    Sheet 3

--- FormalisingMathematics2024/Section01logic/Sheet3.lean
+++ FormalisingMathematics2024/Section01logic/Sheet3.lean
@@ -33,45 +33,92 @@ and the following tactics may also be useful:
 variable (P Q R : Prop)
 
 example : ¬True → False := by
-  sorry
+  intro t
+  change True → False at t
+  apply t
+  triv
   done
 
 example : False → ¬True := by
-  sorry
+  intro f
+  change True → False
+  intro t
+  exact f
   done
 
 example : ¬False → True := by
-  sorry
+  intro nf
+  change False → False at nf
+  by_contra nt
+  change True → False at nt
+  apply nt
+  triv
   done
 
 example : True → ¬False := by
-  sorry
+  intro t
+  change False → False
+  intro f
+  exact f
   done
 
 example : False → ¬P := by
-  sorry
+  intro f
+  change P → False
+  intro p
+  exact f
   done
 
 example : P → ¬P → False := by
-  sorry
+  intro p
+  intro np
+  change P → False at np
+  apply np
+  exact p
   done
 
 example : P → ¬¬P := by
-  sorry
+  intro p
+  intro np
+  apply np
+  exact p
   done
 
 example : (P → Q) → ¬Q → ¬P := by
-  sorry
+  intro pq
+  intro nq
+  intro p
+  by_cases hp : P
+  apply pq at hp
+  apply nq
+  exact hp
+  apply hp
+  exact p
   done
 
 example : ¬¬False → False := by
-  sorry
+  intro nnf
+  apply nnf
+  intro f
+  exact f
   done
 
 example : ¬¬P → P := by
-  sorry
+  intro nnp
+  by_cases np: P
+  exact np
+  apply nnp at np
+  exfalso
+  exact np
   done
 
 example : (¬Q → ¬P) → P → Q := by
-  sorry
+  intro nqnp
+  intro p
+  by_cases hq: Q
+  exact hq
+  by_contra nq
+  apply nqnp at nq
+  apply nq
+  exact p
   done
